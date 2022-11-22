1602df5faefd9905606233342670a929221b73d8
maazbin <maaz.mstqm@gmail.com>
2022-11-22 13:09:31 +0500

    unnecessary code removed, keras helper from pypi used for preprocessing

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import tflite_runtime.interpreter as tflite
 from PIL import Image
 from keras_image_helper import create_preprocessor
 # other modules
-import numpy as np 
 from io import BytesIO
 
 
@@ -25,19 +24,18 @@ app = FastAPI()
 
 def preprocess(file):
 
-    # a = BasePreprocessor.convert_to_tensor()
-    #   reading image daata
+    # read image from  requeset  
     img = file.file.read()
     
     #   mimicing os file support behaviour for bytes stream 
     stream = BytesIO(img)
     
-
+    # stream to image
     img = Image.open(stream)
 
     # creating preprocessor for Xception
     preprocessor = create_preprocessor('xception', target_size=(299, 299))  
-    X = preprocessor.from_loaded_image(img)
+    X = preprocessor.convert_to_tensor(img)
 
 
     return X
@@ -45,19 +43,28 @@ def preprocess(file):
 
 
 def model(X):
+
+    # Creates the TF Lite interpreter (load model)
     interpreter = tflite.Interpreter(model_path='clothing-model-v4.tflite') 
+    
+    # Initializes the interpreter with the model
     interpreter.allocate_tensors() 
     
-
+    # Gets the input: the part of the network that takes in the array X
     input_details = interpreter.get_input_details()
     input_index = input_details[0]['index'] 
     
+    # Gets the output: the part of the network with final predictions
     output_details = interpreter.get_output_details()
     output_index = output_details[0]['index'] 
     
+    # Puts X into the input
     interpreter.set_tensor(input_index, X)
     
+    # Runs the model to get predictions
     interpreter.invoke()
+
+    # Gets the predictions from the output
     preds = interpreter.get_tensor(output_index) 
     
     return preds
@@ -66,6 +73,7 @@ def model(X):
     
     
 def decode(preds):
+    
     labels = [
     'dress',
     'hat',
@@ -78,6 +86,8 @@ def decode(preds):
     'skirt',
     't-shirt'
     ]
+
+    # combine labels with prediction results
     results = dict(zip(labels, preds[0]))
     
     
@@ -87,10 +97,6 @@ def decode(preds):
     return max_value
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
-
 
 @app.post("/")
 async def get_prediction_root(file: UploadFile = File(...)):
@@ -99,30 +105,3 @@ async def get_prediction_root(file: UploadFile = File(...)):
     # preds = prediction(interpreter)
     decode_pred = decode(preds)
     return {"prediction" : decode_pred}
-
-# preprocessor = create_preprocessor('xception', target_size=(299, 299))
-
-
-# interpreter = tflite.Interpreter(model_path='clothing-model-v4.tflite')
-# interpreter.allocate_tensors()
-
-# input_details = interpreter.get_input_details()
-# input_index = input_details[0]['index']
-
-# output_details = interpreter.get_output_details()
-# output_index = output_details[0]['index']
-
-
-# def predict(X):
-#     interpreter.set_tensor(input_index, X)
-#     interpreter.invoke()
-
-#     preds = interpreter.get_tensor(output_index)
-#     return preds[0]
-
-
-
-
-# def decode_predictions(pred):
-#     result = {c: float(p) for c, p in zip(labels, pred)}
-#     return result
